e50425310c765cdc3aaaf1e44b1ad33981750f1d
Dhanushka_105105 <dhanushka.hewakaluannakkage@axiatadigitallabs.com>
2020-11-15 12:29:55 +0530

    created the base project

---
+++ tmfapi-datamodel/src/main/java/com/iit/msc/ase/tmfapidatamodel/TmfapiDatamodelApplication.java
@@ -0,0 +1,13 @@
+package com.iit.msc.ase.tmfapidatamodel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TmfapiDatamodelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TmfapiDatamodelApplication.class, args);
+	}
+
+}
---
+++ tmfapi-datamodel/src/test/java/com/iit/msc/ase/tmfapidatamodel/TmfapiDatamodelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.iit.msc.ase.tmfapidatamodel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TmfapiDatamodelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
