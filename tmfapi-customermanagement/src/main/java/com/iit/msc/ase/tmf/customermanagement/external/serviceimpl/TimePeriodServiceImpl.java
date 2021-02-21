098d36036f2bf6b3ee605b82257372f71255f5e4
Dhanushka_105105 <dhanushka.hewakaluannakkage@axiatadigitallabs.com>
2021-02-21 10:21:15 +0530

    initial steps v18

--- tmfapi-customermanagement/src/main/java/com/iit/msc/ase/tmf/customermanagement/external/serviceimpl/TimePeriodServiceImpl.java
+++ tmfapi-customermanagement/src/main/java/com/iit/msc/ase/tmf/customermanagement/external/serviceimpl/TimePeriodServiceImpl.java
@@ -4,21 +4,18 @@ import com.iit.msc.ase.tmf.customermanagement.domain.boundary.repository.TimePer
 import com.iit.msc.ase.tmf.customermanagement.domain.boundary.service.TimePeriodService;
 import com.iit.msc.ase.tmf.customermanagement.domain.model.TimePeriod;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.stereotype.Service;
 
-@ComponentScan(basePackages = {"com.iit.msc.ase.tmf.customermanagement.domain.boundary.repository.TimePeriodRepository"})
 @Service
 public class TimePeriodServiceImpl implements TimePeriodService {
 
-//    @Autowired
-//    private TimePeriodRepository timePeriodRepository;
+    @Autowired
+    private TimePeriodRepository timePeriodRepository;
 
     @Override
     public TimePeriod create(TimePeriod timePeriod) {
         log("create method of TimePeriod started");
-//        return timePeriodRepository.save(timePeriod);
-        return null;
+        return timePeriodRepository.save(timePeriod);
     }
 
 }
