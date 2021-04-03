b71742a6521e6a8259dba8d0e011d8399b51a4ae
Dhanushka_105105 <dhanushka.hewakaluannakkage@axiatadigitallabs.com>
2021-04-03 13:37:00 +0530

    refactor the code v5

--- tmfapi-datamodel/src/main/java/com/iit/msc/ase/tmf/datamodel/domain/dto/PaymentRefDto.java
+++ tmfapi-datamodel/src/main/java/com/iit/msc/ase/tmf/datamodel/domain/dto/PaymentRefDto.java
@@ -1,6 +1,5 @@
 package com.iit.msc.ase.tmf.datamodel.domain.dto;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
