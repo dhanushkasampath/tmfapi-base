e132418608e08c23e4236e705c83b0f9c9fdb80f
H.K.D.Sampath Kumara <sampath.kumara@aiesec.net>
2021-04-10 19:20:17 +0530

    refactored the pom file to create jars

--- tmfapi-datamodel/src/test/java/com/iit/msc/ase/tmf/datamodel/TmfapiDatamodelApplicationTests.java
+++ tmfapi-datamodel/src/test/java/com/iit/msc/ase/tmf/datamodel/TmfapiDatamodelApplicationTests.java
@@ -1,13 +1,9 @@
 package com.iit.msc.ase.tmf.datamodel;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class TmfapiDatamodelApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
 
 }
